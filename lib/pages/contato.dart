496a22dc6eb73026344606864aa01c13bce9f345
Caio Halbert <caio.halbert@gmail.com>
2020-07-13 16:36:45 -0300

    contato atualizado

--- lib/pages/contato.dart
+++ lib/pages/contato.dart
@@ -107,7 +107,88 @@ class Contato extends StatelessWidget
       {
         return Scaffold(
           backgroundColor: Colors.white,
-          appBar: deskTablet(context)
+          appBar: deskTablet(context),
+          body: Stack(
+            children: <Widget>[
+              Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.only(bottom: 40.0),
+                color: Colors.white,
+                child: Image(
+                  image: AssetImage('assets/images/Contato.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: SizedBox(
+                  width: 540.0,
+                  height: 600.0,
+                  child: Container(
+                    padding: EdgeInsets.fromLTRB(10.0, 10.0, 100.0, 10.0),
+                    color: Colors.white,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                       children: <Widget>
+                       [
+                         Text("CONTATO",
+                         textAlign: TextAlign.center,
+                         style: TextStyle(fontSize: 70.0, fontFamily: "Lora"),
+                         ),
+                         Text("e",
+                         textAlign: TextAlign.center,
+                         style: TextStyle(fontSize: 18.0),
+                         ),
+                         Text("Redes Sociais",
+                         textAlign: TextAlign.center,
+                         style: TextStyle(fontSize: 22.0),
+                         ),
+                         Container
+                         (
+                           child: SizedBox
+                           (
+                              width: 530.0,
+                              height: 350.0,
+                             child: Container
+                             (
+                               padding: EdgeInsets.fromLTRB(10.0, 10.0, 30.0, 10.0),
+                               decoration: BoxDecoration
+                               (
+                                 border: Border.all(color: Colors.black, width: 3)
+                               ),
+                               child: Column
+                               (
+                                 crossAxisAlignment: CrossAxisAlignment.end,
+                                 mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                 children: <Widget>
+                                 [
+                                    Text("E-MAIL\ncontato.claudiarayara@gmail.com",
+                                    textAlign: TextAlign.right,
+                                    style: TextStyle(fontSize: 18.0),
+                                    ),
+                                    
+                                    Text("INSTAGRAM\n@darkmooncrow",
+                                    textAlign: TextAlign.right,
+                                    style: TextStyle(fontSize: 18.0),
+                                    ),
+                                    
+                                    Text("fACEBOOK\n@darkmooncrow",
+                                    textAlign: TextAlign.right,
+                                    style: TextStyle(fontSize: 18.0),
+                                    ),
+                                    
+                                 ],
+                               ),
+                             ),
+                           ),
+                         ),
+                       ],
+                    ),
+                  ),
+                ),
+              ),
+            ] 
+          ),
         );
       }
 
@@ -120,7 +201,79 @@ class Contato extends StatelessWidget
             backgroundColor: Colors.white,
             elevation: 0.0,
           ),
-          drawer: mobile(context)
+          drawer: mobile(context),
+          body: Stack(
+            children: <Widget>[
+              Align(
+                alignment: Alignment.center,
+                child: SizedBox(
+                  width: 540.0,
+                  height: 600.0,
+                  child: Container(
+                    padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+                    color: Colors.white,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                       children: <Widget>
+                       [
+                         Text("CONTATO",
+                         textAlign: TextAlign.center,
+                         style: TextStyle(fontSize: 70.0, fontFamily: "Lora"),
+                         ),
+                         Text("e",
+                         textAlign: TextAlign.center,
+                         style: TextStyle(fontSize: 18.0),
+                         ),
+                         Text("Redes Sociais",
+                         textAlign: TextAlign.center,
+                         style: TextStyle(fontSize: 22.0),
+                         ),
+                         Container
+                         (
+                           child: SizedBox
+                           (
+                              width: 530.0,
+                              height: 350.0,
+                             child: Container
+                             (
+                               padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+                               decoration: BoxDecoration
+                               (
+                                 border: Border.all(color: Colors.black, width: 3)
+                               ),
+                               child: Column
+                               (
+                                 crossAxisAlignment: CrossAxisAlignment.end,
+                                 mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                 children: <Widget>
+                                 [
+                                    Text("E-MAIL\ncontato.claudiarayara@gmail.com",
+                                    textAlign: TextAlign.right,
+                                    style: TextStyle(fontSize: 18.0),
+                                    ),
+                                    
+                                    Text("INSTAGRAM\n@darkmooncrow",
+                                    textAlign: TextAlign.right,
+                                    style: TextStyle(fontSize: 18.0),
+                                    ),
+                                    
+                                    Text("fACEBOOK\n@darkmooncrow",
+                                    textAlign: TextAlign.right,
+                                    style: TextStyle(fontSize: 18.0),
+                                    ),
+                                    
+                                 ],
+                               ),
+                             ),
+                           ),
+                         ),
+                       ],
+                    ),
+                  ),
+                ),
+              ),
+            ] 
+          ),
         );
       }
     });
