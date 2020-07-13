3087807fecbf754c7fe7f39b367938453261a86f
Caio Halbert <caio.halbert@gmail.com>
2020-07-13 16:51:54 -0300

    inicio da criação do blog

--- lib/pages/blog.dart
+++ lib/pages/blog.dart
@@ -1,6 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:darkmooncrow/widgets/header.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:responsive_builder/responsive_builder.dart';
+
+final GoogleSignIn googleSignIn = GoogleSignIn();
+final StorageReference storageRef = FirebaseStorage.instance.ref();
+final usersRef = Firestore.instance.collection("users");
+final postRef = Firestore.instance.collection("posts");
+final commentsRef = Firestore.instance.collection("comments");
 
 class Blog extends StatelessWidget {
   const Blog({Key key}) : super(key: key);
